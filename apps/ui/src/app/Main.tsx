12fe6b3684e122ccf2866916727c236a635cfbf1
Philip Andrew Wee De Wang <philip@firejet.io>
2024-10-30 10:26:08 +0800

    Base sample complete

--- apps/ui/src/app/Main.tsx
+++ apps/ui/src/app/Main.tsx
@@ -8,16 +8,18 @@ export function Main() {
   );
 
   return (
-    <div className="flex flex-col">
-      <span>
+    <div className="flex flex-col gap-2">
+      <span className='mb-4 font-bold'>
         You can test keyboard shortcuts within the webview panel with
         ctrl+shift+l
       </span>
-      <span>
+      <span className='font-bold'>
         Click the below button to get the current contents of the active text
-        editor, as a demo for messaging passing
+        editor, as a demo for messaging passing. The button also can activated with the keyboard shortcut `ctrl + shift + t`, and holding `ctrl` shows a hint for the keyboard shortcut
       </span>
+      <span className="font-mono whitespace-pre">{fileContents}</span>
       <ButtonWithHotkey
+        className="p-2 outline rounded-md self-start outline-1"
         keys="ctrl+shift+t"
         action={async () => {
           const newContents = await trpc.files.getCurContents.query();
@@ -26,8 +28,6 @@ export function Main() {
       >
         Get Contents
       </ButtonWithHotkey>
-      <span>cur file contents:</span>
-      <span className="font-mono">{fileContents}</span>
     </div>
   );
 }
